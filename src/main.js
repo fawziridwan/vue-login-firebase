3888a3cf2f83901e2116e474ac0148f055d35d9b
fawziridwan <fawziridwan@gmail.com>
2019-10-22 19:25:48 +0700

    update minor

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import './plugins/vuetify'
+import router from '@/router'
+import store from '@/store'
+import * as firebase from 'firebase'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+var config = {
+  apiKey: "AIzaSyAPHmLvRh-Tx-VXeivyPxHs82t_cQdRgfA",
+  authDomain: "vue-firebase-95e29.firebaseapp.com",
+  databaseURL: "https://vue-firebase-95e29.firebaseio.com",
+  projectId: "vue-firebase-95e29",
+  storageBucket: "gs://vue-firebase-95e29.appspot.com",
+  messagingSenderId: "288007976915"
+}
+firebase.initializeApp(config)
